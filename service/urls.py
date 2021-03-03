462676d558a94b33ec52164200535fdc311fe976
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-03 23:45:53 +0700

    Добавление корневого файла urls с адресами для обработки запросов

---
+++ service/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from authors.views import AuthorModelViewSet
+
+# Создаём объект класса DefaultRouter и связываем AuthorModelViewSet с адресом authors.
+router = DefaultRouter()
+router.register('authors', AuthorModelViewSet)
+
+urlpatterns = [
+   path('admin/', admin.site.urls),
+   path('api-auth/', include('rest_framework.urls')),
+   path('api/', include(router.urls)),  # Подключаем адреса (urls), которые формирует роутер, к нашему проекту.
+]
