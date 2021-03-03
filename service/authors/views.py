6de5ec8198b84a7cea9bcbc42f201ac67b9f8b09
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-03 23:26:05 +0700

    Создание представления

--- service/authors/views.py
+++ service/authors/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Author
+from .serializers import AuthorModelSerializer
 
-# Create your views here.
+
+class AuthorModelViewSet(ModelViewSet): # Мы используем наследование от ModelViewSet.
+                                        # Это означает, что набор views связан с моделью и будет работать с её данными.
+
+   queryset = Author.objects.all() # queryset указывает, какие данные мы будем выводить в списке.
+   serializer_class = AuthorModelSerializer # serializer_class определяет тот Serializer, который мы будем использовать.
