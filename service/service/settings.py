c798a282a111317d5bc2eadec5615149b4529123
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-04 21:05:58 +0700

    Настройка политики CORS pip install django-cors-headers

--- service/service/settings.py
+++ service/service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authors',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'service.urls'
@@ -120,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+   "http://localhost:3000",
+]
