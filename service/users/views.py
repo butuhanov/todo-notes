fbb8851f5af6ec117ef8ff50844c0f0f44ec138a
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-04 12:17:23 +0700

    Создание представления

--- service/users/views.py
+++ service/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import UserModelSerializer
 
-# Create your views here.
+
+class UserModelViewSet(ModelViewSet): # Мы используем наследование от ModelViewSet.
+                                        # Это означает, что набор views связан с моделью и будет работать с её данными.
+
+   queryset = User.objects.all() # queryset указывает, какие данные мы будем выводить в списке.
+   serializer_class = UserModelSerializer # serializer_class определяет тот Serializer, который мы будем использовать.
