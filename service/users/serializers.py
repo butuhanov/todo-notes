1eafd2159189a63b83f929f0ab9389031d472e3c
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-04 12:16:32 +0700

    добавление сериализатора (исправил опечатку)

--- service/users/serializers.py
+++ service/users/serializers.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import User
 
 
-class AuthorModelSerializer(HyperlinkedModelSerializer):
+class UserModelSerializer(HyperlinkedModelSerializer):
    class Meta:
        model = User  # сериализатор на основании неё будет создавать JSON-представление
        fields = 'username, firstname, lastname, email' # какие поля из модели будут отображаться в json.
