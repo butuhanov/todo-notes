cd30c31fb1969dc1e58a3cb160187560f61f91fd
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-04 12:23:37 +0700

    Исправление ошибки TypeError at /api/users/ The `fields` option must be a list or tuple or "__all__". Got str.

--- service/users/serializers.py
+++ service/users/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
    class Meta:
        model = User  # сериализатор на основании неё будет создавать JSON-представление
-       fields = 'username, firstname, lastname, email' # какие поля из модели будут отображаться в json.
+       fields = 'username', 'firstname', 'lastname', 'email' # какие поля из модели будут отображаться в json.
