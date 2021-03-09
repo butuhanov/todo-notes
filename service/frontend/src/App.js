023b1b65fdc6cf02b5f43a2e83925226257b254b
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-09 17:24:56 +0700

    Создание компонента верхнего уровня в модуле app.js

--- service/frontend/src/App.js
+++ service/frontend/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'; // Импортируем React из библиотеки react
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {  // Создадим класс App, наследуем его от React.Component.
+                                     // Компонент App — это класс, имеющий состояние.
+   constructor(props) {    // В конструктор класса передаётся объект props
+       super(props)        // super(props) — вызывает родительский конструктор
+       this.state = {      // this.state — это объект состояния нашего компонента.
+           'users': []   // Он будет хранить массив пользователей, которые мы будем получать с back-end.
+
+       }
+   }
+
+   render () {     // Метод render отвечает за отрисовку нашего компонента.
+       return (
+           <div>
+               Main App
+           </div>
+       )
+   }
 }
 
-export default App;
+export default App;   // Экспортируем наш компонент для использования в других модулях.
