8db433bf27f5720143563f4cc676e6897b8534fa
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-19 15:44:21 +0700

    Добавление сериализаторов и связей между ними

--- service/authors/management/commands/serializers_example.py
+++ service/authors/management/commands/serializers_example.py
@@ -2,7 +2,6 @@ import io
 
 from rest_framework import serializers
 
-# from python_models import Author
 
 from django.core.management.base import BaseCommand
 
@@ -38,6 +37,32 @@ class Article:
        self.author = author
 
 
+class AuthorSerializer(serializers.Serializer):
+    # AuthorSerializer содержит простые типы полей.
+    name = serializers.CharField(max_length=128)
+    birthday_year = serializers.IntegerField()
+
+
+class BiographySerializer(serializers.Serializer):
+    text = serializers.CharField(max_length=1024)
+    #  в поле author мы указали AuthorSerializer.
+    #  Таким образом, после сериализации получим вложенный словарь с данными автора.
+    author = AuthorSerializer()
+
+class BookSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=128)
+    #  в поле author мы указали AuthorSerializer.
+    #  и добавили к нему дополнительный ключ many=True, так как книгу могут написать несколько авторов.
+    #  После сериализации получим вложенный список, состоящий из словарей с данными автора.
+    authors = AuthorSerializer(many=True)
+
+class ArticleSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=128)
+    #  в поле author мы указали AuthorSerializer.
+    #  Таким образом, после сериализации получим вложенный словарь с данными автора.
+    author = AuthorSerializer()
+
+
 class Command(BaseCommand):
     help = 'serializer example'
 
@@ -49,93 +74,39 @@ class Command(BaseCommand):
       # сериализатор напоминает уже знакомые нам Django Forms: Form — не связана с моделью, а ModelForm — связана.
 
 
-      class AuthorSerializer(serializers.Serializer):
-         name = serializers.CharField(max_length=128)
-         birthday_year = serializers.IntegerField()
+      author1 = Author('Грин', 1880)
+      serializer = AuthorSerializer(author1)
+      print(serializer.data)  # {'name': 'Грин', 'birthday_year': 1880}
 
-         # В метод create передаётся validated_data — словарь валидных данных, а возвращается объект Author.
-         def create(self, validated_data):
-             return Author(**validated_data)
-
-         # В метод update тоже передаётся validated_data, и вместе с ним instance — объект, который мы хотим изменить.
-         def update(self, instance, validated_data):
-             instance.name = validated_data.get('name', instance.name)
-             instance.birthday_year = validated_data.get('birthday_year', instance.birthday_year)
-             return instance
-
-
-         # Как и в Django Forms, в Serializers можно добавить дополнительную валидацию по одному или нескольким полям.
-         #  Для добавления валидации по некоторому полю в serializer включается
-         #  метод validate_<имя поля> (validate_birthday_year).
-         #  Чтобы применить это к нескольким полям — метод validate.
-         def validate_birthday_year(self, value):
-             if value < 0:
-                 # Для генерации ошибки используется класс serializers.ValidationError.
-                 raise serializers.ValidationError('Год рождения не может быть отрицательным')
-             return value
-
-         def validate(self, attrs):
-             if attrs['name'] == 'Пушкин' and attrs['birthday_year'] != 1799:
-                 raise serializers.ValidationError('Неверный год рождения Пушкина')
-             return attrs
-
-      # При передаче только словаря данных и вызова метода save будет вызван метод create.
-      data = {'name': 'Грин', 'birthday_year': 1880}
-      serializer = AuthorSerializer(data=data)
-      # Перед каждым сохранением объекта нужно вызывать метод is_valid serializer-а
-      # для проверки данных, иначе возникнет ошибка.
-      serializer.is_valid()
-      author = serializer.save()
-
-      # При передаче объекта author и словаря данных объект изменится и будет вызван метод update.
-      data = {'name': 'Александр', 'birthday_year': 1880}
-      serializer = AuthorSerializer(author, data=data)
-      serializer.is_valid()
-      author = serializer.save()
-
-      # data = {'name': 'Грин', 'birthday_year': 'abc'}
-      # serializer = AuthorSerializer(data=data)
-      # print(serializer.is_valid())  # False
-
-      # Пример проверки валидации
-      # data = {'name': 'Пушкин', 'birthday_year': 1880}
-      # serializer = AuthorSerializer(author, data=data)
-      # serializer.is_valid()
-      # author = serializer.save()
-      #
-      # data = {'name': 'Александр', 'birthday_year': -10}
-      # serializer = AuthorSerializer(author, data=data)
-      # serializer.is_valid()
-      # author = serializer.save()
-
+      biography = Biography('Текст биографии', author1)
+      serializer = BiographySerializer(biography)
       print(
-          serializer.errors)  # {'birthday_year': [ErrorDetail(string='A valid integer is required.', code='invalid')]}
+          serializer.data)  # {'text': 'Текст биографии', 'author': OrderedDict([('name', 'Грин'), ('birthday_year', 1880)])}
+
+      article = Article('Некоторая статья', author1)
+      serializer = ArticleSerializer(article)
+      print(
+          serializer.data)  # {'name': 'Некоторая статья', 'author': OrderedDict([('name', 'Грин'), ('birthday_year', 1880)])}
+
+      author2 = Author('Пушкин', 1799)
+      book = Book('Некоторая книга', [author1,
+                                      author2])  # {'name': 'Некоторая книга', 'authors': [OrderedDict([('name', 'Грин'), ('birthday_year', 1880)]), OrderedDict([('name', 'Пушкин'), ('birthday_year', 1799)])]}
+
+      serializer = BookSerializer(book)
+      print(serializer.data)
+
+
+      # print(
+      #     serializer.errors)  # {'birthday_year': [ErrorDetail(string='A valid integer is required.', code='invalid')]}
 
       # При невалидных данных возникнет ошибка, если вызвать метод is_valid и
       # передать в него параметр raise_exception=True.
-      serializer.is_valid(raise_exception=True)
+      # serializer.is_valid(raise_exception=True)
 
       # По умолчанию для обновления объекта нужно передать все поля сериализатора (name и birthday_year).
       # Если же мы хотим передать только несколько полей, а остальные оставить как есть,
       # то нужно использовать параметр partial=True.
-      data = {'birthday_year': 2000}
-      serializer = AuthorSerializer(author, data=data, partial=True)
-      serializer.is_valid()
-      author = serializer.save()
 
-
-
-      # Далее, если у нас есть объект класса Author, можем передать его в AuthorSerializer и создать объект serializer.
-
-      author = Author('Грин', 1880)
-      serializer = AuthorSerializer(author)
-      print(serializer.data)  # {'name': 'Грин', 'birthday_year': 1880}
-      # serializer.data — это представление объекта в виде словаря python, который содержит простые типы, доступные в python.
-      print(type(serializer.data))  # <class 'rest_framework.utils.serializer_helpers.ReturnDict'>
-      # Если проверить type(serializer.data), то увидим специальный тип DRF.
-      # Он, в свою очередь, возвращает словарь.
-      # Можно опустить этот момент и говорить, что serializer превращает сложный объект в словарь python,
-      # содержащий простые типы данных.
 
       from rest_framework.renderers import JSONRenderer
       renderer = JSONRenderer()
