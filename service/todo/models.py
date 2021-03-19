00190b2ea087774ed8887b230926fe4caa6732d6
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-19 23:52:55 +0700

    Добавление модели Project

--- service/todo/models.py
+++ service/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+from service.users.models import User
+
+
+class Project(models.Model):
+   uuid = models.UUIDField(primary_key=True, default=uuid4)
+   name = models.CharField(max_length=64) # название проекта
+   repo_link = models.URLField() # ссылка на репозиторий
+
+   # у одного проекта может быть несколько пользователей, поэтому используем отношение "один ко многим"
+   # Одна главная сущность может быть связана с несколькими зависимыми
+   # models.CASCADE: автоматически удаляет строку из зависимой таблицы,
+   # если удаляется связанная строка из главной таблицы
+   # models.PROTECT: блокирует удаление строки из главной таблицы,
+   # если с ней связаны какие-либо строки из зависимой таблицы
+   users = models.ForeignKey(User, on_delete=models.PROTECT)
