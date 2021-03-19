2b5c3a5ea025918a25a4b7cc4b459c12f9aa14a6
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-19 18:43:03 +0700

    Настройка ModelSerializer и вложенных полей. Добавлен пример.

--- service/authors/serializers.py
+++ service/authors/serializers.py
@@ -54,3 +54,22 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'
 
+author1 = Author.objects.create(name='Грин', birthday_year=1880)
+serializer = AuthorSerializer(author1)
+print(serializer.data)  # {'id': 17, 'name': 'Грин', 'birthday_year': 1880}
+
+biography = Biography.objects.create(text='Некоторая биография', author=author1)
+serializer = BiographySerializer(biography)
+print(serializer.data)  # {'text': 'Некоторая биография', 'author': 17}
+
+article = Article.objects.create(name='Некоторая статья', author=author1)
+serializer = ArticleSerializer(article)
+print(serializer.data)  # {'id': 8, 'author': OrderedDict([('id', 17), ('name', 'Грин'), ('birthday_year', 1880)])}
+
+author2 = Author.objects.create(name='Пушкин', birthday_year=1799)
+book = Book.objects.create(name='Некоторая книга')
+book.authors.add(author1)
+book.authors.add(author2)
+book.save()
+serializer = BookSerializer(book)
+print(serializer.data)  # {'id': 9, 'authors': ['Грин', 'Пушкин'], 'name': 'Некоторая книга'}
