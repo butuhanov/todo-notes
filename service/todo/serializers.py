ed5a4adb36678a785d0f55141cc8f305b9692e8c
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-22 09:12:09 +0700

    Добавление сериализаторов

---
+++ service/todo/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from rest_framework.serializers import HyperlinkedModelSerializer
+from .models import Todo, Project
+from service.users.serializers import UserModelSerializer
+
+class TodoSerializer(serializers.ModelSerializer):
+    # Модель To do связана с User, а в сериализаторе мы указали users = UserModelSerializer().
+   # В этом случае за отображение будет отвечать UserModelSerializer(), и на выходе появится вложенный словарь.
+   users = UserModelSerializer()
+   class Meta:
+       model = Todo
+       fields = '__all__'  # отображать все поля модели, включая вложенные.
+
+class ProjectSerializer(serializers.ModelSerializer):
+   # Модель Project связана с User и To do
+   project = TodoSerializer()
+   user = UserModelSerializer()
+
+   class Meta:
+       model = Project
+       fields = '__all__'  # отображать все поля модели, включая вложенные.
+
