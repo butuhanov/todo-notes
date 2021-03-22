e8ce018745ecd2e905804cd7cd70deaea79784c4
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-22 10:12:31 +0700

    Исправление ошибки в сериализаторе Original exception text was: 'Todo' object has no attribute 'users'.

--- service/todo/serializers.py
+++ service/todo/serializers.py
@@ -4,12 +4,12 @@ from .models import Todo, Project
 from users.serializers import UserModelSerializer
 
 class TodoSerializer(serializers.ModelSerializer):
-    # Модель To do связана с User, а в сериализаторе мы указали users = UserModelSerializer().
+    # Модель To do связана с User, а в сериализаторе мы указали user = UserModelSerializer().
    # В этом случае за отображение будет отвечать UserModelSerializer(), и на выходе появится вложенный словарь.
-   users = UserModelSerializer()
+   user = UserModelSerializer()
    class Meta:
        model = Todo
-       fields = '__all__'  # отображать все поля модели, включая вложенные.
+       fields = 'project', 'name', 'created', 'updated', 'user'
 
 class ProjectSerializer(serializers.ModelSerializer):
    # Модель Project связана с User и To do
