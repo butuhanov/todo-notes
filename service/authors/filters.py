98a22f75789c20aa26b1b10cbe9ec97ea4bbee1e
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-22 21:02:52 +0700

    Добавил комментарий

--- service/authors/filters.py
+++ service/authors/filters.py
@@ -3,6 +3,9 @@ from .models import Article
 
 
 class ArticleFilter(filters.FilterSet):
+    # указываем модель, для которой создаётся фильтр, список доступных полей, настройку для каждого поля.
+    # name = filters.CharFilter(lookup_expr='contains') в этом примере мы указали,
+    # что к фильтру в поле name нужно добавить contains для поиска по части имени, а не по полному совпадению.
    name = filters.CharFilter(lookup_expr='contains')
 
    class Meta:
