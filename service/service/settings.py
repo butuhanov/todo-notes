9c471edda8c19ca1e44d5c518e8d94e1459b96a9
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-22 21:24:03 +0700

    Настройка пагинации по умолчанию для всех Views и Viewsets

--- service/service/settings.py
+++ service/service/settings.py
@@ -133,5 +133,7 @@ CORS_ALLOWED_ORIGINS = [
 
 # DRF
 REST_FRAMEWORK = {
-   'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+   'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+   'PAGE_SIZE': 5,
 }
