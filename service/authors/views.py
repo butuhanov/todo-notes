fa6e94de0062a81520f7ede6f98e8c31dcec4ff2
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-22 14:53:05 +0700

    Добавление Views наследуясь от APIView

--- service/authors/views.py
+++ service/authors/views.py
@@ -1,10 +1,33 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
+from .models import Article
+from rest_framework.response import Response
+
 from .models import Author
-from .serializers import AuthorModelSerializer
+from .serializers import AuthorSerializer, ArticleSerializer
 
 
 class AuthorModelViewSet(ModelViewSet): # Мы используем наследование от ModelViewSet.
                                         # Это означает, что набор views связан с моделью и будет работать с её данными.
 
    queryset = Author.objects.all() # queryset указывает, какие данные мы будем выводить в списке.
-   serializer_class = AuthorModelSerializer # serializer_class определяет тот Serializer, который мы будем использовать.
+   serializer_class = AuthorSerializer # serializer_class определяет тот Serializer, который мы будем использовать.
+
+class ArticleAPIVIew(APIView):
+    # Рассмотрим для начала класс APIView.
+    # Это базовый класс для Views в DRF.
+    # Он может быть связан с другими частями DRF (например, Renderers)
+    # и позволяет полностью самостоятельно написать код обработки того или иного запроса.
+
+    # Свойство renderer_classes позволяет указать список Renderers.
+   renderer_classes = [JSONRenderer]
+
+    # Метод get отвечает за get-запрос.
+    # Мы получаем все статьи, с помощью ArticleSerializer
+    # преобразуем выборку в простые типы данных и возвращаем объект Response.
+    # Важно! Для ответа используется объект класса Respose из DRF, а не из django.
+   def get(self, request, format=None):
+       articles = Article.objects.all()
+       serializer = ArticleSerializer(articles, many=True)
+       return Response(serializer.data)
