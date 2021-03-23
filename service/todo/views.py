dbe1672bf1ba4a62bc0634c2d3d314fdd0906fa3
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-23 12:17:15 +0700

    Установил размер страницы 10 записей для постраничного вывода

--- service/todo/views.py
+++ service/todo/views.py
@@ -1,8 +1,16 @@
+from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet
 from todo.models.todo import Todo
 from todo.models.project import Project
 from .serializers import TodoSerializer, ProjectSerializer
 
+
+
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+   default_limit = 10
 
 class TodoModelViewSet(ModelViewSet): # Мы используем наследование от ModelViewSet.
                                         # Это означает, что набор views связан с моделью и будет работать с её данными.
@@ -19,3 +27,5 @@ class ProjectModelViewSet(ModelViewSet):  # Мы используем насле
     queryset = Project.objects.all()  # queryset указывает, какие данные мы будем выводить в списке.
     serializer_class = ProjectSerializer  # serializer_class определяет тот Serializer, который мы будем использовать.
     filter_class = ProjectFilter
+    pagination_class = ProjectLimitOffsetPagination
+
