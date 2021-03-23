cb66f22ca1ba99846c9d60a0a51e3b6a8da727cc
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-23 11:08:17 +0700

    Добавил фильтр по пользователям Есть возможность просмотра списка и каждого пользователя в отдельности

---
+++ service/users/filters.py
@@ -0,0 +1,10 @@
+from django_filters import rest_framework as filters
+from .models import User
+
+
+class UserFilter(filters.FilterSet):
+   username = filters.CharFilter(lookup_expr='contains')
+
+   class Meta:
+       model = User
+       fields = ['username']
