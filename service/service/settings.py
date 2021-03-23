ef68a7d09df8fa7ca9b257acdc5cfcf8fd96fad8
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-23 16:41:12 +0700

    Указываем права по умолчанию для всего проекта

--- service/service/settings.py
+++ service/service/settings.py
@@ -136,4 +136,7 @@ REST_FRAMEWORK = {
    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
    'PAGE_SIZE': 5,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
