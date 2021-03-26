e5511986a72fc757a599cd626f601eea8392c000
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-03-26 16:10:31 +0700

    Прописал настройки для dynaconf

--- service/service/settings.py
+++ service/service/settings.py
@@ -192,3 +192,9 @@ LOGGING = {
 
     }
 }
+
+# HERE STARTS DYNACONF EXTENSION LOAD (Keep at the very bottom of settings.py)
+# Read more at https://dynaconf.readthedocs.io/en/latest/guides/django.html
+import dynaconf  # noqa
+settings = dynaconf.DjangoDynaconf(__name__)  # noqa
+# HERE ENDS DYNACONF EXTENSION LOAD (No more code below this line)
