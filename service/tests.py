6c779185f47f8a9a1a877fbb4f4a42e66efd498f
butuhanov <36451784+butuhanov@users.noreply.github.com>
2021-04-08 15:12:01 +0700

    Добавил файл в котором будут находиться тесты

---
+++ service/tests.py
@@ -0,0 +1,9 @@
+import json
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from todo.views import TodoModelViewSet
+from todo.models import Todo, Project
+
